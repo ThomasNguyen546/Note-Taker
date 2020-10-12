231bce6cd80df6fa879a8c8c3213bfe921e13da3
Thomas Nguyen <thomasnguyen546@gmail.com>
2020-10-11 19:06:26 -0600

    fixed db file

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,11 +1,10 @@
 const router = require('express').Router();
 const { createNewNote, deleteNote } = require('../../lib/notes');
-const { notes } = require('../../data/db.json');
+const { notes } = require('../../db/db.json');
 
 router.get('/notes', (req, res) => {
     res.json(notes);
 });
-
 
 router.post('/notes', (req, res) => {
     req.body.id = notes.length.toString();
